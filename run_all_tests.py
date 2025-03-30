92b70ceab95238db8a173f148f3dff0b35b18a8f
Krishna <seandean125@gmail.com>
2025-03-30 17:48:15 -0400

    remove char \u2705 checkmark

--- run_all_tests.py
+++ run_all_tests.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     text = test.extract_shadow_roots_and_text()
     
     if text == "One Platform to Meet, Manage, and Close.":
-        print("✅ Visual Test Case Passed")
+        print(" Visual Test Case Passed")
     else:
-        print("❌ Visual Test Case Failed")
+        print(" Visual Test Case Failed")
     
     test.quit()
 
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     test.open_page(url)
     homepage_url = test.extract_shadow_roots_and_button()
     if homepage_url == "https://linqapp.com/":
-        print("✅ HomePage Button -> Homepage Test Case Passed")
+        print(" HomePage Button -> Homepage Test Case Passed")
     else:
-        print("❌ HomePage Button -> Homepage Test Case Failed")
+        print(" HomePage Button -> Homepage Test Case Failed")
     
     test.quit()
 
@@ -35,9 +35,9 @@ if __name__ == "__main__":
     if shadow_host_level_1:
         apple_url = test.click_apple_btn(shadow_host_level_1)
         if "https://appleid.apple.com/auth/authorize" in apple_url:
-            print("✅ Apple Auth Button Test Case Passed")
+            print(" Apple Auth Button Test Case Passed")
     else:
-        print("❌ Apple Auth Button Test Case Failed")
+        print(" Apple Auth Button Test Case Failed")
     
     test.quit()
 
@@ -49,9 +49,9 @@ if __name__ == "__main__":
         shadow_host_level_1_copy = test.extract_shadow_root()
         staging_url = test.return_to_staging_page(shadow_host_level_1_copy)
         if staging_url == url:
-            print("✅ Email Auth Button Test Case Passed")
+            print(" Email Auth Button Test Case Passed")
     else:
-        print("❌ Email Auth Button Test Case Failed")
+        print(" Email Auth Button Test Case Failed")
     
     test.quit()
 
@@ -61,9 +61,9 @@ if __name__ == "__main__":
     if shadow_host_level_1:
         google_btn_url = test.click_google_btn(shadow_host_level_1)
         if "https://accounts.google.com/v3/signin" in google_btn_url:
-            print("✅ Google Auth Button Test Case Passed")
+            print(" Google Auth Button Test Case Passed")
     else:
-        print("❌ Google Auth Button Test Case Failed")
+        print(" Google Auth Button Test Case Failed")
     
     test.quit()
 
@@ -73,8 +73,8 @@ if __name__ == "__main__":
     if shadow_host_level_1:
         linkedin_btn_url = test.click_linkedin_btn(shadow_host_level_1)
         if "https://www.linkedin.com/uas/login" in linkedin_btn_url:
-            print("✅ Linkedin Auth Button Test Case Passed")
+            print(" Linkedin Auth Button Test Case Passed")
     else:
-        print("❌ Linkedin Auth Button Test Case Failed")
+        print(" Linkedin Auth Button Test Case Failed")
     
     test.quit()
